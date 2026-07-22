c63f627e8b98d783c3b0603d124070975373fc5f
6362prajju <shreekarpuranik3@gmail.com>
2026-07-22 12:08:46 +0530

    Refactor code structure for improved readability and maintainability

---
+++ src/app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const multer = require("multer");
+const uploadFile = require("./services/storage.service")
+const postModel = require("./models/post.model")
+
+const app = express();
+app.use(express.json());
+
+const upload = multer({ storage: multer.memoryStorage() })
+
+app.post("/create-post", upload.single("image"), async(req,res) => {
+
+    const result = await uploadFile(req.file.buffer)
+
+    await postModel.create({
+        image: result.url,
+        caption: req.body.caption,
+    })
+    res.status(201).json({
+        message : "Image uploaded to imagekit successfully"
+    })
+})
+
+app.get("/posts", async(req,res) => {
+    const posts = await postModel.find();
+    res.status(200).json({
+        message: "fetched successfully",
+        posts : posts
+    })
+})
+
+module.exports = app;
